90e16889b8277a6c69f6f716028bf7a96c95818e
Jaime Llanos <jllanosb@unc.edu.pe>
2025-09-09 18:59:01 -0500

    Agregando Ejercicio condicional multiple

---
+++ E3_Multiples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algoritmo Condicional Multiple");
+            /*
+             Vocales para ayudar al niño Dislexico
+             */
+
+            Console.WriteLine("Ingrese un numero [1-5]");
+            var num = int.Parse(Console.ReadLine());
+
+            switch (num) {
+                case 1: Console.WriteLine("La vocal es la A"); break;
+                case 2: Console.WriteLine("La vocal es la E"); break;
+                case 3: Console.WriteLine("La vocal es la I"); break;
+                case 4: Console.WriteLine("La vocal es la O"); break;
+                case 5: Console.WriteLine("La vocal es la U"); break;
+                default: Console.WriteLine("Numero incorrecto"); break;
+            }
+
+        }
+    }
+}
